afe7fa95f943b45a4ece5db9450f6b5f12dd4c49
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-07-15 14:37:11 +0100

    dynamically add terraform imports based on environment

---
+++ bin/terraform_imports/import_prod.tf
@@ -0,0 +1,79 @@
+import {
+  id = "D3MGU4E8Yox9KUisBig2gXgrAtjiuxkd"
+  to = auth0_client.copilot_credits
+}
+
+import {
+  id = "NrJ4a0Ng6JVT7CHoDBsmDSg238FMXMOW"
+  to = auth0_client.auth0_actions_management_client
+}
+
+import {
+  id = "8MiAvhSz97LbeiaIsDPLU4hmR7n9k98G"
+  to = auth0_client.auth0_terraform_provider
+}
+
+import {
+  id = "cgr_ci8KbFDNy1DqQPc4"
+  to = auth0_client_grant.auth0_terraform_provider_client_grant
+}
+
+import {
+  id = "cgr_eUtGpCVkQhK40rWE"
+  to = auth0_client_grant.auth0_actions_management_client_grant
+}
+
+import {
+  id = "con_u00XnA8QzGohAmni"
+  to = auth0_connection.github
+}
+
+import {
+  id = "con_u00XnA8QzGohAmni"
+  to = auth0_connection_clients.github
+}
+
+import {
+  id = "b90e14dc-5495-410b-8f02-741dd700566a"
+  to = auth0_action.restrict_by_github_org
+}
+
+import {
+  id = "post-login"
+  to = auth0_trigger_actions.post_login
+}
+
+import {
+  id = "0a18bb89-7e5e-455b-8834-8a798a51f49b"
+  to = auth0_tenant.tenant
+}
+
+import {
+  id = "6a3aaff464551f7d0b3dd425"
+  to = auth0_resource_server.auth0_management_api
+}
+
+import {
+  id = "e56e7391-0fbb-4e46-904b-18e16836d7af"
+  to = auth0_attack_protection.attack_protection
+}
+
+import {
+  id = "e7320a14-e8aa-4f4b-9465-b159eb4c7c2e"
+  to = auth0_guardian.guardian
+}
+
+import {
+  id = "D3MGU4E8Yox9KUisBig2gXgrAtjiuxkd"
+  to = auth0_client_credentials.copilot_credits
+}
+
+import {
+  id = "NrJ4a0Ng6JVT7CHoDBsmDSg238FMXMOW"
+  to = auth0_client_credentials.auth0_actions_management_client
+}
+
+import {
+  id = "8MiAvhSz97LbeiaIsDPLU4hmR7n9k98G"
+  to = auth0_client_credentials.auth0_terraform_provider
+}
