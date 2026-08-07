cf9fb1ea14b976798eeed75f715ad79b73f0ece3
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-08-07 14:11:43 +0100

    test only org membership logic

--- terraform/auth0/actions_code/enforce_github_itdentity.js
+++ terraform/auth0/actions_code/enforce_github_itdentity.js
@@ -39,17 +39,19 @@ exports.onExecutePostLogin = async (event, api) => {
       return api.access.deny(`Access Denied: You are not a member of an authorised organisation:$${allowedOrgs.join(', ')}`);
     };
 
-    // Set user application role
-    const githubRole = await vghp.assignUserRole(access_token, coreGitHubOrg, adminTeam, githubUsername);
+    // // Set user application role
+    // const githubRole = await vghp.assignUserRole(access_token, coreGitHubOrg, adminTeam, githubUsername);
 
-    // Dev: Verify user membership of approved teams
-    if ("${environment}" == "development") {
-      const isTeamMember = await vghp.checkTeamMembershipAtLeastOne(access_token, githubUsername, coreGitHubOrg, devTeamSlugs);
+    // // Dev: Verify user membership of approved teams
+    // if ("${environment}" == "development") {
+    //   const isTeamMember = await vghp.checkTeamMembershipAtLeastOne(access_token, githubUsername, coreGitHubOrg, devTeamSlugs);
 
-      if (!isTeamMember) {
-        return api.access.deny('Access Denied: You are not authorized under the required GitHub teams.');
-      }
-    }
+    //   if (!isTeamMember) {
+    //     return api.access.deny('Access Denied: You are not authorized under the required GitHub teams.');
+    //   }
+    // }
+
+    githubRole = "admin";
 
     // Add user role to user session token
     await management.users.update(
